84054fa3bf6aeaae48bc589534311be8b499a96b
Zwyx <29386932+Zwyx@users.noreply.github.com>
2025-11-07 22:54:01 +0800

    Set up tooling
    taken from the `vscode-autoclosetabs` project.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,5 @@
-# Change Log
+# Changelog
 
-All notable changes to the "remote-trash" extension will be documented in this file.
+## 1.0.0
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
-
-- Initial release
+Initial release of Remove Trash.
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,82 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import eslint from "@eslint/js";
+import configPrettier from "eslint-config-prettier";
+import pluginOnlyWarn from "eslint-plugin-only-warn";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+export default defineConfig({
+	extends: [
+		eslint.configs.recommended,
+		tseslint.configs.strictTypeChecked,
+		configPrettier,
+	],
+	files: ["**/*.ts"],
+	languageOptions: {
+		ecmaVersion: 2022,
+		sourceType: "module",
+		parserOptions: {
+			projectService: true,
+		},
+	},
+	plugins: {
+		"only-warn": pluginOnlyWarn,
+	},
+	rules: {
+		// ---------- JavaScript ----------
 
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
+		// Enforce consistent brace style for all control statements
+		curly: "warn",
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+		// Require `===` when `==` can be ambiguous
+		eqeqeq: ["warn", "always"],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+		// Disallow the use of `console.log`, which helps not forget them after debugging; for permanent logging, use `console.info/warn/warn`
+		"no-console": ["warn", { allow: ["info", "warn", "error"] }],
+
+		// Disallow the use of `eval()`; if `eval()` is necessary, use `// eslint-disable-next-line no-eval` where it's needed
+		"no-eval": "warn",
+
+		// Disallow `new` operators with the `Function` object, as this is similar to `eval()`; if necessary, use `// eslint-disable-next-line no-new-func` where it's needed
+		"no-new-func": "warn",
+
+		// Disallow reassigning function parameters
+		"no-param-reassign": "warn",
+
+		"no-restricted-syntax": [
+			"warn",
+			{
+				// "selector": ":not(BinaryExpression:matches([operator='!=='], [operator='==='])) > Literal[value=null]",
+				selector: "Literal[value=null]",
+				message: "Don't use 'null', use 'undefined'.",
+			},
+		],
+
+		// Disallow ternary operators when simpler alternatives exist; example: prevent `const x = y === 1 ? true : false` in favour of `const x = y === 1`
+		"no-unneeded-ternary": "warn",
+
+		// Disallow renaming import, export, and destructured assignments to the same name; example: prevent `const { a: a } = b;` in favour of `const { a } = b;`
+		"no-useless-rename": "warn",
+
+		// Disallow throwing anything else than the `Error object`
+		"no-throw-literal": "warn",
+
+		// Require method and property shorthand syntax for object literals; example: prevent `a = { b: b };` in favour of `a = { b };`
+		"object-shorthand": "warn",
+
+		// ---------- TypeScript ----------
+
+		// Enforce consistent usage of type imports
+		// Will activate when VS Code's feature "organise imports" manages that automatically
+		// "@typescript-eslint/consistent-type-imports": "warn",
+
+		// Disallow variable declarations from shadowing variables declared in the outer scope; necessitates to disable `no-shadow`, see https://typescript-eslint.io/rules/no-shadow
+		"no-shadow": "off",
+		"@typescript-eslint/no-shadow": "warn",
+
+		// Disallow certain types in boolean expressions
+		"@typescript-eslint/strict-boolean-expressions": [
+			"warn",
+			{ allowNullableBoolean: true },
+		],
+	},
+});
